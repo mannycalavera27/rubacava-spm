0ce54c0a2b7292585e40ba149e8cbcb3d358156b
mannycalavera27 <tiziano.cialfi@soft.it>
2021-01-18 09:18:04 +0100

    dequeueReusableCell public

--- Sources/Rubacava/UITableView+Ext.swift
+++ Sources/Rubacava/UITableView+Ext.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UITableView {
-    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
+    public func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Unable to dequeue reusable table view cell")
         }

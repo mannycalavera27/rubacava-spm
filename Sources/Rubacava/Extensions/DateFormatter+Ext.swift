6747cd63ff7050d9266564e019507ef0b20e43db
mannycalavera27 <tiziano.cialfi@soft.it>
2021-01-27 10:09:34 +0100

    Network Manager: date formatter extensions set as public

---
+++ Sources/Rubacava/Extensions/DateFormatter+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tiziano Cialfi on 27/01/21.
+//
+
+import Foundation
+
+public extension DateFormatter {
+    static let standardWithTSeparator: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        return df
+    }()
+    
+    static let acf: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+        return df
+    }()
+    
+}

e14d7429c1b48815a21d83e4e423cf3f5380d244
mannycalavera27 <tiziano.cialfi@soft.it>
2021-02-15 17:08:10 +0100

    add RCCollectionViewController

---
+++ Sources/Rubacava/Subclasses/RCCollectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RCCollectionViewController.swift
+//  
+//
+//  Created by Tiziano Cialfi on 15/02/21.
+//
+
+import UIKit
+
+public class RCCollectionViewController: UICollectionViewController {
+    
+    public init(scrollDirection: UICollectionView.ScrollDirection = .vertical) {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = scrollDirection
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}

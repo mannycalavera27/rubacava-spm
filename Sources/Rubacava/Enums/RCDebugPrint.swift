aff300e58ad6ca3aea9a2de246614952f0a7e0ed
mannycalavera27 <tiziano.cialfi@soft.it>
2021-03-11 11:31:12 +0100

    RCDebugPrint debug control moved

--- Sources/Rubacava/Enums/RCDebugPrint.swift
+++ Sources/Rubacava/Enums/RCDebugPrint.swift
@@ -8,14 +8,16 @@
 import Foundation
 
 public enum RCDebugPrint {
-    #if DEBUG
+    
     public static func location(_ message: Any,
                          filePath: String = #filePath,
                          function: String = #function,
                          line: Int = #line,
                          column: Int = #column) {
+        #if DEBUG
         let fileName = (filePath as NSString).lastPathComponent
         print("\(message)\n\t\(fileName)\n\t\(function)\n\tline: \(line), column: \(column)")
+        #endif
     }
-    #endif
+
 }
